769aee354c18be69b6f5c0ea42a57c463a86ee50
MadibiyevAmir <amir.madibiyev@narxoz.kz>
2025-04-05 10:34:00 +0500

    RealImage.java

--- proxy-pattern/src/RealImage.java
+++ proxy-pattern/src/RealImage.java
@@ -5,3 +5,18 @@ public class RealImage implements Image {
         this.filename = filename;
         loadFromDisk();
     }
+
+    private void loadFromDisk() {
+        System.out.println("Loading full-resolution image: " + filename);
+    }
+
+    @Override
+    public void displayThumbnail() {
+        System.out.println("Displaying thumbnail for: " + filename);
+    }
+
+    @Override
+    public void displayFullImage() {
+        System.out.println("Displaying full-resolution image: " + filename);
+    }
+}
